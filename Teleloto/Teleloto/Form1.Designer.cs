21f7577bf1182b171ff88f5ea8153637ba01edbb
cinis77 <cinis.dwarnius.minis@gmail.com>
2018-11-22 21:22:47 +0200

    Add files via upload

---
+++ Teleloto/Teleloto/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Teleloto
+{
+    partial class Teleloto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GroupBilietas = new System.Windows.Forms.GroupBox();
+            this.M1 = new System.Windows.Forms.TextBox();
+            this.M2 = new System.Windows.Forms.TextBox();
+            this.M3 = new System.Windows.Forms.TextBox();
+            this.M4 = new System.Windows.Forms.TextBox();
+            this.M5 = new System.Windows.Forms.TextBox();
+            this.bGenerate = new System.Windows.Forms.Button();
+            this.J5 = new System.Windows.Forms.TextBox();
+            this.J4 = new System.Windows.Forms.TextBox();
+            this.J3 = new System.Windows.Forms.TextBox();
+            this.J2 = new System.Windows.Forms.TextBox();
+            this.J1 = new System.Windows.Forms.TextBox();
+            this.G5 = new System.Windows.Forms.TextBox();
+            this.G4 = new System.Windows.Forms.TextBox();
+            this.G3 = new System.Windows.Forms.TextBox();
+            this.G2 = new System.Windows.Forms.TextBox();
+            this.G1 = new System.Windows.Forms.TextBox();
+            this.R5 = new System.Windows.Forms.TextBox();
+            this.R4 = new System.Windows.Forms.TextBox();
+            this.R3 = new System.Windows.Forms.TextBox();
+            this.R2 = new System.Windows.Forms.TextBox();
+            this.R1 = new System.Windows.Forms.TextBox();
+            this.Z5 = new System.Windows.Forms.TextBox();
+            this.Z4 = new System.Windows.Forms.TextBox();
+            this.Z3 = new System.Windows.Forms.TextBox();
+            this.Z2 = new System.Windows.Forms.TextBox();
+            this.Z1 = new System.Windows.Forms.TextBox();
+            this.textNumbers = new System.Windows.Forms.TextBox();
+            this.bplay = new System.Windows.Forms.Button();
+            this.GroupBilietas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GroupBilietas
+            // 
+            this.GroupBilietas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.GroupBilietas.Controls.Add(this.Z5);
+            this.GroupBilietas.Controls.Add(this.Z4);
+            this.GroupBilietas.Controls.Add(this.Z3);
+            this.GroupBilietas.Controls.Add(this.Z2);
+            this.GroupBilietas.Controls.Add(this.Z1);
+            this.GroupBilietas.Controls.Add(this.R5);
+            this.GroupBilietas.Controls.Add(this.R4);
+            this.GroupBilietas.Controls.Add(this.R3);
+            this.GroupBilietas.Controls.Add(this.R2);
+            this.GroupBilietas.Controls.Add(this.R1);
+            this.GroupBilietas.Controls.Add(this.G5);
+            this.GroupBilietas.Controls.Add(this.G4);
+            this.GroupBilietas.Controls.Add(this.G3);
+            this.GroupBilietas.Controls.Add(this.G2);
+            this.GroupBilietas.Controls.Add(this.G1);
+            this.GroupBilietas.Controls.Add(this.J5);
+            this.GroupBilietas.Controls.Add(this.J4);
+            this.GroupBilietas.Controls.Add(this.J3);
+            this.GroupBilietas.Controls.Add(this.J2);
+            this.GroupBilietas.Controls.Add(this.J1);
+            this.GroupBilietas.Controls.Add(this.bGenerate);
+            this.GroupBilietas.Controls.Add(this.M5);
+            this.GroupBilietas.Controls.Add(this.M4);
+            this.GroupBilietas.Controls.Add(this.M3);
+            this.GroupBilietas.Controls.Add(this.M2);
+            this.GroupBilietas.Controls.Add(this.M1);
+            this.GroupBilietas.Location = new System.Drawing.Point(34, 33);
+            this.GroupBilietas.Name = "GroupBilietas";
+            this.GroupBilietas.Size = new System.Drawing.Size(492, 439);
+            this.GroupBilietas.TabIndex = 0;
+            this.GroupBilietas.TabStop = false;
+            this.GroupBilietas.Text = "Bilietas";
+            // 
+            // M1
+            // 
+            this.M1.Location = new System.Drawing.Point(21, 59);
+            this.M1.Name = "M1";
+            this.M1.Size = new System.Drawing.Size(38, 35);
+            this.M1.TabIndex = 0;
+            // 
+            // M2
+            // 
+            this.M2.Location = new System.Drawing.Point(21, 111);
+            this.M2.Name = "M2";
+            this.M2.Size = new System.Drawing.Size(38, 35);
+            this.M2.TabIndex = 1;
+            // 
+            // M3
+            // 
+            this.M3.Location = new System.Drawing.Point(21, 164);
+            this.M3.Name = "M3";
+            this.M3.Size = new System.Drawing.Size(38, 35);
+            this.M3.TabIndex = 2;
+            // 
+            // M4
+            // 
+            this.M4.Location = new System.Drawing.Point(21, 221);
+            this.M4.Name = "M4";
+            this.M4.Size = new System.Drawing.Size(38, 35);
+            this.M4.TabIndex = 3;
+            // 
+            // M5
+            // 
+            this.M5.Location = new System.Drawing.Point(21, 280);
+            this.M5.Name = "M5";
+            this.M5.Size = new System.Drawing.Size(38, 35);
+            this.M5.TabIndex = 4;
+            // 
+            // bGenerate
+            // 
+            this.bGenerate.Location = new System.Drawing.Point(21, 355);
+            this.bGenerate.Name = "bGenerate";
+            this.bGenerate.Size = new System.Drawing.Size(129, 51);
+            this.bGenerate.TabIndex = 5;
+            this.bGenerate.Text = "Generate";
+            this.bGenerate.UseVisualStyleBackColor = true;
+            this.bGenerate.Click += new System.EventHandler(this.bGenerate_Click);
+            // 
+            // J5
+            // 
+            this.J5.Location = new System.Drawing.Point(75, 280);
+            this.J5.Name = "J5";
+            this.J5.Size = new System.Drawing.Size(38, 35);
+            this.J5.TabIndex = 10;
+            // 
+            // J4
+            // 
+            this.J4.Location = new System.Drawing.Point(75, 221);
+            this.J4.Name = "J4";
+            this.J4.Size = new System.Drawing.Size(38, 35);
+            this.J4.TabIndex = 9;
+            // 
+            // J3
+            // 
+            this.J3.Location = new System.Drawing.Point(75, 164);
+            this.J3.Name = "J3";
+            this.J3.Size = new System.Drawing.Size(38, 35);
+            this.J3.TabIndex = 8;
+            // 
+            // J2
+            // 
+            this.J2.Location = new System.Drawing.Point(75, 111);
+            this.J2.Name = "J2";
+            this.J2.Size = new System.Drawing.Size(38, 35);
+            this.J2.TabIndex = 7;
+            // 
+            // J1
+            // 
+            this.J1.Location = new System.Drawing.Point(75, 59);
+            this.J1.Name = "J1";
+            this.J1.Size = new System.Drawing.Size(38, 35);
+            this.J1.TabIndex = 6;
+            // 
+            // G5
+            // 
+            this.G5.Location = new System.Drawing.Point(132, 280);
+            this.G5.Name = "G5";
+            this.G5.Size = new System.Drawing.Size(38, 35);
+            this.G5.TabIndex = 15;
+            // 
+            // G4
+            // 
+            this.G4.Location = new System.Drawing.Point(132, 221);
+            this.G4.Name = "G4";
+            this.G4.Size = new System.Drawing.Size(38, 35);
+            this.G4.TabIndex = 14;
+            // 
+            // G3
+            // 
+            this.G3.Location = new System.Drawing.Point(132, 164);
+            this.G3.Name = "G3";
+            this.G3.Size = new System.Drawing.Size(38, 35);
+            this.G3.TabIndex = 13;
+            // 
+            // G2
+            // 
+            this.G2.Location = new System.Drawing.Point(132, 111);
+            this.G2.Name = "G2";
+            this.G2.Size = new System.Drawing.Size(38, 35);
+            this.G2.TabIndex = 12;
+            // 
+            // G1
+            // 
+            this.G1.Location = new System.Drawing.Point(132, 59);
+            this.G1.Name = "G1";
+            this.G1.Size = new System.Drawing.Size(38, 35);
+            this.G1.TabIndex = 11;
+            // 
+            // R5
+            // 
+            this.R5.Location = new System.Drawing.Point(189, 280);
+            this.R5.Name = "R5";
+            this.R5.Size = new System.Drawing.Size(38, 35);
+            this.R5.TabIndex = 20;
+            // 
+            // R4
+            // 
+            this.R4.Location = new System.Drawing.Point(189, 221);
+            this.R4.Name = "R4";
+            this.R4.Size = new System.Drawing.Size(38, 35);
+            this.R4.TabIndex = 19;
+            // 
+            // R3
+            // 
+            this.R3.Location = new System.Drawing.Point(189, 164);
+            this.R3.Name = "R3";
+            this.R3.Size = new System.Drawing.Size(38, 35);
+            this.R3.TabIndex = 18;
+            // 
+            // R2
+            // 
+            this.R2.Location = new System.Drawing.Point(189, 111);
+            this.R2.Name = "R2";
+            this.R2.Size = new System.Drawing.Size(38, 35);
+            this.R2.TabIndex = 17;
+            // 
+            // R1
+            // 
+            this.R1.Location = new System.Drawing.Point(189, 59);
+            this.R1.Name = "R1";
+            this.R1.Size = new System.Drawing.Size(38, 35);
+            this.R1.TabIndex = 16;
+            // 
+            // Z5
+            // 
+            this.Z5.Location = new System.Drawing.Point(252, 280);
+            this.Z5.Name = "Z5";
+            this.Z5.Size = new System.Drawing.Size(38, 35);
+            this.Z5.TabIndex = 25;
+            // 
+            // Z4
+            // 
+            this.Z4.Location = new System.Drawing.Point(252, 221);
+            this.Z4.Name = "Z4";
+            this.Z4.Size = new System.Drawing.Size(38, 35);
+            this.Z4.TabIndex = 24;
+            // 
+            // Z3
+            // 
+            this.Z3.Location = new System.Drawing.Point(252, 164);
+            this.Z3.Name = "Z3";
+            this.Z3.Size = new System.Drawing.Size(38, 35);
+            this.Z3.TabIndex = 23;
+            // 
+            // Z2
+            // 
+            this.Z2.Location = new System.Drawing.Point(252, 111);
+            this.Z2.Name = "Z2";
+            this.Z2.Size = new System.Drawing.Size(38, 35);
+            this.Z2.TabIndex = 22;
+            // 
+            // Z1
+            // 
+            this.Z1.Location = new System.Drawing.Point(252, 59);
+            this.Z1.Name = "Z1";
+            this.Z1.Size = new System.Drawing.Size(38, 35);
+            this.Z1.TabIndex = 21;
+            // 
+            // textNumbers
+            // 
+            this.textNumbers.Location = new System.Drawing.Point(34, 512);
+            this.textNumbers.Multiline = true;
+            this.textNumbers.Name = "textNumbers";
+            this.textNumbers.Size = new System.Drawing.Size(492, 210);
+            this.textNumbers.TabIndex = 1;
+            // 
+            // bplay
+            // 
+            this.bplay.Location = new System.Drawing.Point(402, 740);
+            this.bplay.Name = "bplay";
+            this.bplay.Size = new System.Drawing.Size(107, 58);
+            this.bplay.TabIndex = 2;
+            this.bplay.Text = "Play!";
+            this.bplay.UseVisualStyleBackColor = true;
+            this.bplay.Click += new System.EventHandler(this.bplay_Click);
+            // 
+            // Teleloto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 839);
+            this.Controls.Add(this.bplay);
+            this.Controls.Add(this.textNumbers);
+            this.Controls.Add(this.GroupBilietas);
+            this.Name = "Teleloto";
+            this.Text = "Teleloto";
+            this.GroupBilietas.ResumeLayout(false);
+            this.GroupBilietas.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GroupBilietas;
+        private System.Windows.Forms.Button bGenerate;
+        private System.Windows.Forms.TextBox M5;
+        private System.Windows.Forms.TextBox M4;
+        private System.Windows.Forms.TextBox M3;
+        private System.Windows.Forms.TextBox M2;
+        private System.Windows.Forms.TextBox M1;
+        private System.Windows.Forms.TextBox Z5;
+        private System.Windows.Forms.TextBox Z4;
+        private System.Windows.Forms.TextBox Z3;
+        private System.Windows.Forms.TextBox Z2;
+        private System.Windows.Forms.TextBox Z1;
+        private System.Windows.Forms.TextBox R5;
+        private System.Windows.Forms.TextBox R4;
+        private System.Windows.Forms.TextBox R3;
+        private System.Windows.Forms.TextBox R2;
+        private System.Windows.Forms.TextBox R1;
+        private System.Windows.Forms.TextBox G5;
+        private System.Windows.Forms.TextBox G4;
+        private System.Windows.Forms.TextBox G3;
+        private System.Windows.Forms.TextBox G2;
+        private System.Windows.Forms.TextBox G1;
+        private System.Windows.Forms.TextBox J5;
+        private System.Windows.Forms.TextBox J4;
+        private System.Windows.Forms.TextBox J3;
+        private System.Windows.Forms.TextBox J2;
+        private System.Windows.Forms.TextBox J1;
+        private System.Windows.Forms.TextBox textNumbers;
+        private System.Windows.Forms.Button bplay;
+    }
+}
+
---
+++ Teleloto/Teleloto/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teleloto
+{
+    public partial class Teleloto : Form
+    {
+        public Teleloto()
+        {
+            InitializeComponent();
+        }
+
+        private void bGenerate_Click(object sender, EventArgs e)
+        {
+            TextBox[] Melyni = { M1, M2, M3, M4, M5 };
+            TextBox[] Juodi = { J1, J2, J3, J4, J5 };
+            TextBox[] Geltoni = { G1, G2, G3, G4, G5 };
+            TextBox[] Raudoni = { R1, R2, R3, R4, R5 };
+            TextBox[] Zali = { Z1, Z2, Z3, Z4, Z5 };
+            GenerateRandomColumn(Melyni, 1, 16);
+            GenerateRandomColumn(Juodi, 16, 31);
+            GenerateRandomColumn(Geltoni, 31, 46);
+            GenerateRandomColumn(Raudoni, 46, 61);
+            GenerateRandomColumn(Zali, 61, 76);
+        }
+
+        private void GenerateRandomColumn(TextBox[] column, int start, int finish)
+        {
+            Random rng = new Random();
+            int i = 0;
+            while (i < column.Length)
+            {
+                string skaicius = rng.Next(start, finish).ToString();
+                bool vienodi = false;
+                foreach (var item in column)
+                {
+                    if (item.Text == skaicius)
+                    {
+                        vienodi = true;
+                        break;
+                    }
+                }
+                if (!vienodi)
+                {
+                    column[i].Text = skaicius;
+                    i++;
+                }
+            }
+        }
+
+        private void bplay_Click(object sender, EventArgs e)
+        {
+            Random rng = new Random();
+            TextBox[] _textboxContainer = { M1, M2, M3, M4, M5,
+                J1, J2, J3, J4, J5,
+                G1, G2, G3, G4, G5,
+                R1, R2, R3, R4, R5,
+                Z1, Z2, Z3, Z4, Z5
+        };
+            List<int> skaiciai = new List<int>();
+            int i = 0;
+            while (i < 49)
+            {
+                int temp = rng.Next(1, 76);
+                bool vienodas = false;
+
+                foreach (var item in skaiciai)
+                {
+                    if (item == temp)
+                    {
+                        vienodas = true;
+                        break;
+                    }
+                }
+                if (!vienodas)
+                {
+                    textNumbers.Text += temp + " ";
+                    skaiciai.Add(temp);
+                    CheckNumberOnTicket(_textboxContainer, temp.ToString());
+                    i++;
+                }
+            }
+        }
+
+        private void CheckNumberOnTicket(TextBox[] container, string skaicius)
+        {
+            foreach (var item in container)
+            {
+                if (item.Text == skaicius)
+                {
+                    item.BackColor = Color.Green;
+                }
+            }
+        }
+    }
+}
