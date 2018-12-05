3e677734ba1a353ccd1081a97b8883a2a69af3df
cinis77 <cinis.dwarnius.minis@gmail.com>
2018-12-05 08:35:02 +0200

    Add files via upload

---
+++ ÈxceptionForm/ÈxceptionForm/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ÈxceptionForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bNespausti_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                throw new MemberAccessException(message: "Informacija paspaustas mygtukas");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+                {
+                    throw new Exception(message: "PLease fill both fields");
+                    /// DROP * DATABSE '
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
