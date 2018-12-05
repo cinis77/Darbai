843d1018eddf78e3c768f0f4590dd4c393d59854
cinis77 <cinis.dwarnius.minis@gmail.com>
2018-12-05 08:35:16 +0200

    Add files via upload

---
+++ KoldunuCempionatas/KoldunuCempionatas/Dalyvis.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoldunuCempionatas
+{
+    public class PersivalgymasException : Exception
+    {
+        public string message { get; set; }
+
+        public PersivalgymasException(string message)
+        {
+            this.message = message;
+        }
+
+        public PersivalgymasException()
+        {
+        }
+    }
+
+    internal class Dalyvis
+    {
+        public int ID { get; }
+        public string Name { get; }
+        public int SuvalgytuKoldunuKiekis { get; set; }
+        public int SkrandzioDydis { get; set; }
+
+        public Dalyvis(int ID, string Name, int SuvalgytuKoldunuKiekis, int SkrandzioDydis)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.SuvalgytuKoldunuKiekis = SuvalgytuKoldunuKiekis;
+            this.SkrandzioDydis = SkrandzioDydis;
+        }
+    }
+}
---
+++ KoldunuCempionatas/KoldunuCempionatas/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KoldunuCempionatas;
+using static KoldunuCempionatas.Dalyvis;
+
+namespace KoldunuCempionatas
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            string path = null;
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    path = ofd.FileName;
+                }
+            }
+            List<Dalyvis> dalyviai = new List<Dalyvis>();
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(path))
+            {
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var data = line.Split(' ');
+                    try
+                    {
+                        dalyviai.Add(new Dalyvis(int.Parse(data[0]), data[1], int.Parse(data[2]), int.Parse(data[3])));
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+                DuomenuTikrinimas(dalyviai);
+            }
+        }
+
+        private static void DuomenuTikrinimas(List<Dalyvis> dalyviai)
+        {
+            foreach (var item in dalyviai)
+            {
+                try
+                {
+                    if (item.SkrandzioDydis < item.SuvalgytuKoldunuKiekis)
+                    {
+                        throw new PersivalgymasException("Persivalge");
+                    }
+
+                    double valgymoGreitis = (double)item.SuvalgytuKoldunuKiekis / 60;
+                    Console.WriteLine("{0} dalyvio valgymo greitis {1}", item.Name, valgymoGreitis);
+                }
+                catch (PersivalgymasException e)
+                {
+                    MessageBox.Show(e.message);
+                }
+                catch (DivideByZeroException e)
+                {
+                    MessageBox.Show("Dalyba is nulio negalima");
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Unknown error");
+                }
+            }
+        }
+    }
+}
