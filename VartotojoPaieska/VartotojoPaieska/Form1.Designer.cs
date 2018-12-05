0c65d007f02229aad0b3e1024b6978dd3d31e74e
cinis77 <cinis.dwarnius.minis@gmail.com>
2018-12-05 08:36:23 +0200

    Add files via upload

---
+++ VartotojoPaieska/VartotojoPaieska/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace VartotojoPaieska
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textIeskomasis = new System.Windows.Forms.TextBox();
+            this.textBoxRezultatai = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textIeskomasis
+            // 
+            this.textIeskomasis.Location = new System.Drawing.Point(36, 93);
+            this.textIeskomasis.Name = "textIeskomasis";
+            this.textIeskomasis.Size = new System.Drawing.Size(267, 35);
+            this.textIeskomasis.TabIndex = 0;
+            // 
+            // textBoxRezultatai
+            // 
+            this.textBoxRezultatai.Location = new System.Drawing.Point(568, 74);
+            this.textBoxRezultatai.Multiline = true;
+            this.textBoxRezultatai.Name = "textBoxRezultatai";
+            this.textBoxRezultatai.Size = new System.Drawing.Size(330, 499);
+            this.textBoxRezultatai.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 61);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Paieska";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 851);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxRezultatai);
+            this.Controls.Add(this.textIeskomasis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textIeskomasis;
+        private System.Windows.Forms.TextBox textBoxRezultatai;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ VartotojoPaieska/VartotojoPaieska/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VartotojoPaieska
+{
+    internal class Vartotojas
+    {
+        public string Vardas { get; set; }
+        public string Pavarde { get; set; }
+
+        public Vartotojas(string vardas, string pavarde)
+        {
+            Vardas = vardas;
+            Pavarde = pavarde;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private readonly List<Vartotojas> Vartotojai;
+        private string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Vartotojai = new List<Vartotojas>();
+
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    path = ofd.FileName;
+                }
+            }
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(path))
+            {
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var data = line.Split(' ');
+                    Vartotojai.Add(new Vartotojas(data[0], data[1]));
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var check = Vartotojai.Where(x => x.Vardas == textIeskomasis.Text);
+                if (check.Count() == 0)
+                {
+                    throw new IndexOutOfRangeException("Nerastas Vartotojas");
+                }
+                foreach (var item in textIeskomasis.Text.ToCharArray())
+                {
+                    if (char.IsDigit(item))
+                    {
+                        throw new FormatException();
+                    }
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message);
+                DialogResult dialogResult = MessageBox.
+                    Show("Ar sukurti nauja vartotoja", "Sukurti nauja vartotoja",
+                    MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    using (System.IO.StreamWriter writer = new System.IO.StreamWriter(path, true))
+                    {
+                        writer.WriteLine(textIeskomasis.Text);
+                    }
+                }
+            }
+        }
+    }
+}
