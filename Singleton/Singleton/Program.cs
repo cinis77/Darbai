4b178afcad69bfe185c73f993c26fae79a4a2f7c
cinis77 <cinis.dwarnius.minis@gmail.com>
2018-12-13 20:03:57 +0200

    Add files via upload

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class SingletonExample
+    {
+        public static SingletonExample instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void Method()
+        {
+        }
+
+        public void Method1()
+        {
+        }
+
+        public void Method2()
+        {
+        }
+
+        public void Method3()
+        {
+        }
+
+        public void Method4()
+        {
+        }
+
+        public void Method5()
+        {
+        }
+
+        public void Method6()
+        {
+        }
+
+        public void Method7()
+        {
+        }
+
+        public void Method8()
+        {
+        }
+
+        public void Method9()
+        {
+        }
+
+        public void Method10()
+        {
+        }
+
+        public void Method11()
+        {
+        }
+
+        public void Method12()
+        {
+        }
+
+        public void Method13()
+        {
+        }
+
+        protected SingletonExample()
+        {
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            SingletonExample.instance.Method(); // Instance == Method
+            Console.WriteLine();
+            Convert.ToInt32();
+            Math.Asin();
+        }
+    }
+}
